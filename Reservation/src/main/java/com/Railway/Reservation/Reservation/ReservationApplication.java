d3bd71022b703ee15a1dd2c849f8292aea7ff81d
svidya98 <110025906+svidya98@users.noreply.github.com>
2022-07-26 16:23:44 +0530

    Add files via upload

---
+++ Reservation/src/main/java/com/Railway/Reservation/Reservation/ReservationApplication.java
@@ -0,0 +1,12 @@
+package com.Railway.Reservation.Reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBoot
+public class ReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationApplication.class, args);
+	}
+
+}
